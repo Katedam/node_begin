e99a5d84988eaa45ce95827c146c94532f700139
Kate Dameron <katerj33@gmail.com>
2019-01-02 20:46:21 -0800

    asynchronous request

--- server.js
+++ server.js
@@ -4,8 +4,26 @@ var http = require('http');
 //createServer() is a function offered by http. This function returns an
 // object, and this object has a method named listen, and takes a numeric value which indicates the
 // port number our HTTP server is going to listen on
-http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World!");
+
+// http.createServer(function(request, response) {
+//     response.writeHead(200, {"Content-Type": "text/plain"});
+//     response.write("Hello World!");
+//     response.end();
+// }).listen(8888);
+
+//asynchronous 
+//request and response are objects and they have methods that handle the details of the HTTP request and to respond
+function onRequest(request, response) {
+    console.log("Request received. ");
+    // Whenever a request is received, it uses the response.writeHead()
+    // function to send an HTTP status 200 and content-type in the HTTP response header, and the
+    // response.write() function to send the text “Hello World” in the HTTP response body
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write('Hello World');
     response.end();
-}).listen(8888);
+}
+
+http.createServer(onRequest).listen(8888);
+
+console.log('listening on port 8888');
+
