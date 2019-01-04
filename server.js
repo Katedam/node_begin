9a056a4b04261b535d011528ebb81ddc079597da
Kate Dameron <katerj33@gmail.com>
2019-01-03 16:37:47 -0800

    birth of a router

--- server.js
+++ server.js
@@ -1,40 +1,23 @@
 //requires the http module that ships with node.js and makes it accessible through 'http'
 var http = require('http');
-
-//createServer() is a function offered by http. This function returns an
-// object, and this object has a method named listen, and takes a numeric value which indicates the
-// port number our HTTP server is going to listen on
-
-// http.createServer(function(request, response) {
-//     response.writeHead(200, {"Content-Type": "text/plain"});
-//     response.write("Hello World!");
-//     response.end();
-// }).listen(8888);
-
-//asynchronous 
-//request and response are objects and they have methods that handle the details of the HTTP request and to respond
-// function onRequest(request, response) {
-//     console.log("Request received. ");
-    // Whenever a request is received, it uses the response.writeHead()
-    // function to send an HTTP status 200 and content-type in the HTTP response header, and the
-    // response.write() function to send the text “Hello World” in the HTTP response body
-//     response.writeHead(200, {'Content-Type': 'text/plain'});
-//     response.write('Hello World');
-//     response.end();
-// }
-
-// http.createServer(onRequest).listen(8888);
-
-// console.log('listening on port 8888');
+var url = require('url');
 
 //creating and exporting modules
-function start() {
+function start(route) {
     function onRequest(request, response) {
-        console.log("Request received.");
+        var pathname = url.parse(request.url).pathname;
+        console.log('Request for ' + pathname + ' received');
+
+        route(pathname);
+        
         response.writeHead(200, {"Content-Type": "text/plain"});
         response.write("Hello World");
         response.end();
     }
+
+    //createServer() is a function offered by http. This function returns an
+    // object, and this object has a method named listen, and takes a numeric value which indicates the
+    // port number our HTTP server is going to listen on
     http.createServer(onRequest).listen(8888);
     console.log("listening on 8888");
 }
