5f1b67951a42d62e80c87e29679e478a7bb3d427
Kate Dameron <katerj33@gmail.com>
2019-01-03 17:07:09 -0800

    request handlers

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ var http = require('http');
 var url = require('url');
 
 //creating and exporting modules
-function start(route) {
+function start(route, handle) {
     function onRequest(request, response) {
         var pathname = url.parse(request.url).pathname;
         console.log('Request for ' + pathname + ' received');
 
-        route(pathname);
-        
+        route(handle, pathname);
+
         response.writeHead(200, {"Content-Type": "text/plain"});
         response.write("Hello World");
         response.end();
