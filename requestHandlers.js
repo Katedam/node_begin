976efdcedcbc6d038bef3ca325fa4fdb473006e5
Kate Dameron <katerj33@gmail.com>
2019-01-12 17:37:55 -0800

    added eslint

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,42 +1,42 @@
 // var exec = require("child_process").exec;
-var querystring = require("querystring"),
-    fs = require('fs');
+var querystring = require('querystring'),
+  fs = require('fs');
 
 function start(response) {
-    console.log(`Request handler 'start' was called`);
+  console.log('Request handler \'start\' was called');
 
-    var body = '<html>'+
-        '<head>'+
-        '<meta http-equiv="Content-Type" content="text/html/ '+
-        'charset=UTF-8" />'+
-        '</head>'+
-        '<body>'+
-        '<form action="/upload" method="post">'+
-        '<textarea name="text" rows="20" cols="60"></textarea>'+
-        '<input type="submit" value="Submit text" />'+
-        '</form>'+
-        '</body>'+
+  var body = '<html>' +
+        '<head>' +
+        '<meta http-equiv="Content-Type" content="text/html/ ' +
+        'charset=UTF-8" />' +
+        '</head>' +
+        '<body>' +
+        '<form action="/upload" method="post">' +
+        '<textarea name="text" rows="20" cols="60"></textarea>' +
+        '<input type="submit" value="Submit text" />' +
+        '</form>' +
+        '</body>' +
         '</html>';
 
-        response.writeHead(200, {"Content-Type": "text/html"});
-        response.write(body);
-        response.end();
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(body);
+  response.end();
 }
 
 // ADD postData as a param to upload to page
 function upload(response, postData) {
-    console.log(`Request handler 'upload' was called`);
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    //WRITE postData to page
-    response.write("You've sent: " + querystring.parse(postData).text);
-    response.end();
+  console.log('Request handler \'upload\' was called');
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
+  //WRITE postData to page
+  response.write('You\'ve sent: ' + querystring.parse(postData).text);
+  response.end();
 }
 
 
 function show(response) {
-    console.log("Request handler 'show' was called.");
-    response.writeHead(200, {"Content-Type": "image/png"});
-    fs.createReadStream("/tmp/test.png").pipe(response);
+  console.log('Request handler \'show\' was called.');
+  response.writeHead(200, { 'Content-Type': 'image/png' });
+  fs.createReadStream('/tmp/test.png').pipe(response);
 }
 
 
--- server.js
+++ server.js
@@ -2,34 +2,34 @@ var http = require('http');
 var url = require('url');
 
 function start(route, handle) {
-    function onRequest(request, response) {
-        var postData = "";
-        var pathname = url.parse(request.url).pathname;
-        console.log('Request for ' + pathname + ' received');
-        // EXPECT the encoding of the received data to be UTF-8
-        request.setEncoding("utf8");
+  function onRequest(request, response) {
+    var postData = '';
+    var pathname = url.parse(request.url).pathname;
+    console.log('Request for ' + pathname + ' received');
+    // EXPECT the encoding of the received data to be UTF-8
+    request.setEncoding('utf8');
         
-        // STEP-by-step the listener fills 'postData with new chunks of data
-        request.addListener("data", function(postDataChunk) {
-            postData += postDataChunk;
-            console.log("Received POST data chunk '"+
-            postDataChunk + "'.");
-        });
+    // STEP-by-step the listener fills 'postData with new chunks of data
+    request.addListener('data', function(postDataChunk) {
+      postData += postDataChunk;
+      console.log('Received POST data chunk \'' +
+            postDataChunk + '\'.');
+    });
 
-        // ROUTER is only called once all data is gathered so it's in the end event
-        request.addListener("end", function() {
-            route(handle, pathname, response);
-        });
+    // ROUTER is only called once all data is gathered so it's in the end event
+    request.addListener('end', function() {
+      route(handle, pathname, response);
+    });
 
-        // response.writeHead(200, {"Content-Type": "text/plain"});
-    }
+    // response.writeHead(200, {"Content-Type": "text/plain"});
+  }
 
-    //createServer() is a function offered by http. This function returns an
-    // object, and this object has a method named listen, 
-    // and takes a numeric value which indicates the
-    // port number our HTTP server is going to listen on
-    http.createServer(onRequest).listen(8888);
-    console.log("listening on 8888");
+  //createServer() is a function offered by http. This function returns an
+  // object, and this object has a method named listen, 
+  // and takes a numeric value which indicates the
+  // port number our HTTP server is going to listen on
+  http.createServer(onRequest).listen(8888);
+  console.log('listening on 8888');
 }
 
 exports.start = start;
