f0a5d7952d12dcd15b337367370cb7cbf715bf25
Kate Dameron <katerj33@gmail.com>
2019-01-12 18:03:25 -0800

    finished adding formidable elements - pages load but image is just a little square

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,8 @@
+/* eslint-disable no-console */
 // var exec = require("child_process").exec;
 var querystring = require('querystring'),
-  fs = require('fs');
+  fs = require('fs'),
+  formidable = require('formidable');
 
 function start(response) {
   console.log('Request handler \'start\' was called');
@@ -11,9 +13,9 @@ function start(response) {
         'charset=UTF-8" />' +
         '</head>' +
         '<body>' +
-        '<form action="/upload" method="post">' +
-        '<textarea name="text" rows="20" cols="60"></textarea>' +
-        '<input type="submit" value="Submit text" />' +
+        '<form action="/upload" enctype="multipart/form-data" ' + 'method="post">' +
+        '<input type="file" name="upload" multiple="multiple">' +
+        '<input type="submit" value="Upload file" />' +
         '</form>' +
         '</body>' +
         '</html>';
@@ -24,11 +26,25 @@ function start(response) {
 }
 
 // ADD postData as a param to upload to page
-function upload(response, postData) {
+function upload(response, request) {
   console.log('Request handler \'upload\' was called');
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
-  //WRITE postData to page
-  response.write('You\'ve sent: ' + querystring.parse(postData).text);
+
+  var form = new formidable.IncomingForm();
+  console.log('about to parse');
+  form.parse(request, function(error, fields, files) {
+    console.log('parsing done');
+    fs.rename(files.upload.path, '/tmp/test.png', function(error) {
+      if(error) {
+        fs.unlink('/tmp/test.png');
+        fs.rename(files.upload.path, '/tmp/test.png');
+      }
+    });
+    
+  });
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  //WRITE data to page
+  response.write('received image:<br/>');
+  response.write('<img src=\'/show\' />');
   response.end();
 }
 
--- server.js
+++ server.js
@@ -3,25 +3,10 @@ var url = require('url');
 
 function start(route, handle) {
   function onRequest(request, response) {
-    var postData = '';
     var pathname = url.parse(request.url).pathname;
     console.log('Request for ' + pathname + ' received');
     // EXPECT the encoding of the received data to be UTF-8
-    request.setEncoding('utf8');
-        
-    // STEP-by-step the listener fills 'postData with new chunks of data
-    request.addListener('data', function(postDataChunk) {
-      postData += postDataChunk;
-      console.log('Received POST data chunk \'' +
-            postDataChunk + '\'.');
-    });
-
-    // ROUTER is only called once all data is gathered so it's in the end event
-    request.addListener('end', function() {
-      route(handle, pathname, response);
-    });
-
-    // response.writeHead(200, {"Content-Type": "text/plain"});
+    route(handle, pathname, response, request);
   }
 
   //createServer() is a function offered by http. This function returns an
